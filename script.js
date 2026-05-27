9c53c260e088775c83b3d7706ad1934897d6eb4b
teslacoil699 <nickreggiani356@gmail.com>
2026-05-27 17:14:43 +0000

    a lot

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // Main JavaScript file
 
-console.log('Script loaded');
+const exploreBtn = document.querySelector('.btn-primary');
+const featuredSection = document.querySelector('#featured');
+
+if (exploreBtn && featuredSection) {
+  exploreBtn.addEventListener('click', event => {
+    event.preventDefault();
+    featuredSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+}
